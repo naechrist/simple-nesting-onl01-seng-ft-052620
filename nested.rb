e6eb1ff878d16db03a5a7ef2d1caeffd1e1d42ab
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-02 01:04:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,11 +64,10 @@ def adding_matz
 # add the following information to the top level of programmer_hash
  
 	programmer_hash = 
- 		{:yukihiro_matsumoto => {
+ 		:yukihiro_matsumoto => {
   :known_for => "Ruby",
     :languages => ["LISP", "C"]
- }
-
+},
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -81,7 +80,8 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     
+
 
     
 end
